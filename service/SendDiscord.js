aebaefad91d15d3e28896d29d48d2a53e6594b95
joao-takayama <joao.takayama@azi.com.br>
2022-10-21 21:50:50 -0400

    [FEATURE] Envio de msg para discord

--- service/SendDiscord.js
+++ service/SendDiscord.js
@@ -1,17 +1,23 @@
 import axios from "axios";
 
-const webhookDiscordUrl = 'https://discord.com/api/webhooks/1033186207372414998/rtmnjiazLmJHs-zJ69aYh0hJ4YFuuqZIAtfWQYGG5GvvUhCKhgdUlCutCydC_K2ZbY0p'
+const webhookDiscordUrlBeta = 'https://discord.com/api/webhooks/1033186207372414998/rtmnjiazLmJHs-zJ69aYh0hJ4YFuuqZIAtfWQYGG5GvvUhCKhgdUlCutCydC_K2ZbY0p'
+const webhookDiscordUrlWork = 'https://discord.com/api/webhooks/1033195603540320266/wxBa_u2UXCu1tRKj2BcCpxkbFzMMYMBy22_QH5OgjE8JVtIkdqLNH7zMs2y6JNsdqi3A'
 
 export default class SendDiscord {
 
     async send(dataGit) {
-        console.log('chamou')
         const msgDicord = {
-            content: `${dataGit.projectName} 
-            Versão liberada: ${dataGit.tag} 
+            content: `${dataGit.projectName} \n Versão liberada: ${dataGit.tag} 
             \n ${dataGit.message} 
             \n\n Link do projeto: ${dataGit.urlRepository}`
         }
-        await axios.post(webhookDiscordUrl, msgDicord)
+
+        if (dataGit.tag.contains('-')) {
+            await axios.post(webhookDiscordUrlBeta, msgDicord)
+        } else{
+            await axios.post(webhookDiscordUrlWork, msgDicord)
+        }
+
+
     }
 }
